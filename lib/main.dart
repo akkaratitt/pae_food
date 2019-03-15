2f0a6968ff50af7d47ed690c096c098b1533faf0
akkarat <paecpe60@gmail.com>
2019-03-15 15:59:54 +0700

    Finish 15Mar2019 OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,112 @@
 import 'package:flutter/material.dart';
+import 'screens/register.dart';
 
-void main() {
-  runApp(App());
-}
+// void main() {
+//   runApp(App());
+// }
+
+void main() => runApp(App());
 
 class App extends StatelessWidget {
-// Explicit
-  Widget nameApp = Text('Pae Food');
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pae Food',
-      home: nameApp,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+// Explicit
+  Widget nameApp = Text(
+    'Pae Food',
+    style: TextStyle(
+        fontFamily: 'Pacifico',
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.blue[700]),
+  );
+
+  Widget logo = Image.asset('images/car2.png');
+
+  Widget emailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Email Address', hintText: 'you@email.com'),
+    );
+  }
+
+  Widget passwordTextField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Password', hintText: 'more 6 Charator'),
+    );
+  }
+
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.blue[600],
+      child: Text(
+        'Sing In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {
+        print('You Click SingIn');
+      },
+    );
+  }
+
+  Widget signUpButton(BuildContext context) {
+    return RaisedButton(
+      color: Colors.blue[300],
+      child: Text(
+        'Sing Up',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {
+        print('You Click SingUp');
+        var registerRoute = new MaterialPageRoute(
+            builder: (BuildContext context) => Register());
+        Navigator.of(context).push(registerRoute);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.yellow, Colors.green[400]],
+                begin: Alignment.topCenter)),
+        child: Container(
+          margin: EdgeInsets.only(top: 80.0, bottom: 80.0),
+          constraints: BoxConstraints.expand(width: 200.0),
+          child: Column(
+            children: <Widget>[
+              logo,
+              nameApp,
+              emailTextField(),
+              passwordTextField(),
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[signInButton(), signUpButton(context)],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
